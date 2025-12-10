f848396ecc62364fc432f62ecbb56fba40d63ddb
inigomace <inigomace172@gmail.com>
2025-12-10 16:55:44 +0000

    Delete src directory

--- src/firisk/data/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 10 15:18:39 2025
-
-@author: Inigo
-"""
-
--- src/firisk/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 10 15:42:43 2025
-
-@author: Inigo
-"""
-
